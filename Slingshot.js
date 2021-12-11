ee5b69d51a839a6d4dcffc007faaf3fc8a3ebdd3
alexbarberena <91705859+alexbarberena@users.noreply.github.com>
2021-12-10 18:20:44 -0600

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,69 @@
+/*class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 1
+        }
+        this.sling = Constraint.create(options);
+        this.pointB=pointB;
+        World.add(world, this.sling);
+
+    }
+    attach(body){
+        this.sling.bodyA = body;
+    }
+    fly(){
+        this.sling.bodyA =null;
+        
+    }
+    display(){
+        if(this.sling.bodyA){
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB;
+        
+        push();
+        strokeWeight(4);
+        stroke("red");
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        pop();
+         }
+        }
+    
+}*/
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 1
+        }
+        this.pointB = pointB;
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    attach(body){
+        this.sling.bodyA = body;
+    }
+    fly(){
+        
+        this.sling.bodyA=null;
+
+    }
+
+    display(){
+          if(this.sling.bodyA){
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB;
+        push();
+        strokeWeight(4);
+        stroke("red")
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        pop();
+    }
+     
+    }
+    
+}
